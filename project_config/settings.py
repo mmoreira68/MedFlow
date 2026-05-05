743293fc90b086b213c803e71c2d49d18bb06ac1
androsgithub <andreemaprinc@gmail.com>
2026-05-05 20:45:53 -0300

    Ajusta a taxa de limite para usuários anônimos na configuração do Django REST Framework

--- project_config/settings.py
+++ project_config/settings.py
@@ -208,11 +208,8 @@ if DEBUG:
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "DEFAULT_THROTTLE_CLASSES": [
-        "rest_framework.throttling.AnonRateThrottle",
-    ],
     "DEFAULT_THROTTLE_RATES": {
-        "anon": "100/hour",
+        "anon": "250/hour",
     },
 }
 
